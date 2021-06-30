b2859dbfd7b0fe2bb94850133ab79cbbb6278aad
kpEERAWk <kla197914@gmail.com>
2021-06-30 22:47:59 +0300

    ArrayList<>() updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,8 +18,8 @@ public class Main {
                     new Person("Maks", 21, CEO)
             );
 // task 1 - get number of CEOs
-            Long ceo_num =null;
-            ceo_num = team.stream().filter(s->s.getOccupation()==CEO).count();
+            Long ceo_num = null;
+            ceo_num = team.stream().filter(s -> s.getOccupation() == CEO).count();
             System.out.println("We have " + ceo_num + " CEOs running the business!");
 
 //// task 2 - get max age in team
@@ -27,49 +27,50 @@ public class Main {
             max_age = team.stream().mapToInt(Person::getAge).max().getAsInt();
             System.out.println("\nThe oldest person in team is only " + max_age);
 //// task 3 - get all team members per position, except CEO
-            Map<Occupation, List<Person>> positions =  Arrays.stream(Occupation.values())
-                    .filter(e->!e.equals(Occupation.CEO))
-                    .collect(Collectors.toMap(e->e, e-> team.stream().filter(s->s.getOccupation()==e).collect(Collectors.toList())));
+            Map<Occupation, List<Person>> positions = Arrays.stream(Occupation.values())
+                    .filter(e -> !e.equals(Occupation.CEO))
+                    .collect(Collectors.toMap(e -> e, e -> team.stream().filter(s -> s.getOccupation() == e).collect(Collectors.toList())));
 
             positions.forEach((occupation, team_members) -> System.out.println(occupation + ": " + team_members));
 //// Удалить из списка все дублирующиеся значения (оставить только уникальные)
-            Integer numbers[] = {1,2,5,3,5,2,1,1,4,7,3,2};
-            Set<Integer> uniqueNumbers= new HashSet<>(Arrays.asList(numbers));
+            Integer numbers[] = {1, 2, 5, 3, 5, 2, 1, 1, 4, 7, 3, 2};
+            Set<Integer> uniqueNumbers = new HashSet<>(Arrays.asList(numbers));
 
-            System.out.println("\nУникальные значения "+uniqueNumbers);
+            System.out.println("\nУникальные значения " + uniqueNumbers);
 //// Сделать реверс числа (из 12345 в 54321)  num = 12345
             int num = 12345;
             StringBuilder sb = new StringBuilder();
             sb.append(num);
-            sb=sb.reverse();
+            sb = sb.reverse();
             num = Integer.parseInt(sb.toString());
-            System.out.println("\nРеверс числа 12345 = "+num);
+            System.out.println("\nРеверс числа 12345 = " + num);
 //// Скопировать структуру из d1 в d2.
-    //d1 = {"data": {"number": 1}}
-    //d2 = d1.clone
+            //d1 = {"data": {"number": 1}}
+            //d2 = d1.clone
 ////  Выведите все елементы первого списка, которых нет во втором
             String[] l1 = {"White", "Black", "Red"};
             String[] l2 = {"Red", "Green"};
             List<String> list1 = new ArrayList(Arrays.asList(l1));
             List<String> list2 = new ArrayList(Arrays.asList(l2));
 
-            List<String> resultArray = Stream.concat(list1.stream(), list2.stream()).distinct().collect(Collectors.toList());
-            System.out.println("\nEлементы первого списка, которых нет во втором "+resultArray);
+            list1.removeAll(list2);
+            List<String> resultArray = list1;
+            System.out.println("\nEлементы первого списка, которых нет во втором " + resultArray);
 ////Отсортировать список словарей по ключу 'x'
 //            //    l = ["x": 4}, {"x": 2}, {"x": 1}, {"x": 3}]
 //
 //
 ////Вывести все уникальные имена из листа, результат должен быть в апперкейсе. пример: ["PETER","PAUL","STEVE"]
-                    List<PersonAge> personList = new ArrayList<>();
-                    personList.add(new PersonAge("Peter", "Patterson", 21));
-                    personList.add(new PersonAge("Paul", "Walker", 31));
-                    personList.add(new PersonAge("Steve", "Runner", 41));
-                    personList.add(new PersonAge("Arnold", "", -1));
-                    personList.add(new PersonAge(" ", "Stevenson", 19));
-                    personList.add(new PersonAge("Arnold", "Stevenson", 19));
-                    personList.add(null);
-                    personList.add(new PersonAge("Aaron", "Bortnicker", 18));
-                }
-            }
-        }
+            List<PersonAge> personList = new ArrayList<>();
+            personList.add(new PersonAge("Peter", "Patterson", 21));
+            personList.add(new PersonAge("Paul", "Walker", 31));
+            personList.add(new PersonAge("Steve", "Runner", 41));
+            personList.add(new PersonAge("Arnold", "", -1));
+            personList.add(new PersonAge(" ", "Stevenson", 19));
+            personList.add(new PersonAge("Arnold", "Stevenson", 19));
+            personList.add(null);
+            personList.add(new PersonAge("Aaron", "Bortnicker", 18));
+
+
+    }
 }
